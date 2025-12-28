9f6b9f0cf28923a1f3eee3500bc765865dff383e
tkwiatkowskii <tomaszkwiatkowski191@gmail.com>
2025-12-28 01:09:11 +0100

    Added ability to read filtered data from database

--- src/models/product.models/ProductSearchParameters.ts
+++ src/models/product.models/ProductSearchParameters.ts
@@ -1,5 +1,10 @@
 export default class ProductSearchParameters {
-  public category: string = "ANY";
-  public searchFilter: string = "ANY";
+  public category: string = "any";
+  public name: string = "any";
   public page: number = 0;
+  public sortType: sortParameters = "name";
+  public sortOrder: sortOrder = "asc"
 }
+
+type sortParameters = "name" | "price";
+type sortOrder = "asc" | "desc";
---
+++ src/services/product.services/getProductsService.ts
@@ -0,0 +1,56 @@
+import { Op } from "sequelize";
+import ProductSearchParameters from "../../models/product.models/ProductSearchParameters.js";
+import { Product } from "../../infrastructure/models/index.js";
+import ProductDto from "../../models/product.models/ProductDto.js";
+import ProductsSummaryDto from "../../models/product.models/ProductsSummaryDto.js";
+
+export default async function getProductsService(
+    searchParams: ProductSearchParameters,
+    retries = 1
+  ): Promise<ProductsSummaryDto> {
+
+    const whereClause: any = {};
+    if (searchParams.category !== 'any') {
+      whereClause.category = { [Op.iLike]: searchParams.category };
+    }
+
+    if (searchParams.name !== 'any') {
+      whereClause.name = { [Op.iLike]: `%${searchParams.name}%` };
+    }
+
+    const querySortOrder = searchParams.sortOrder;
+    const querySortType = searchParams.sortType;
+
+    const PAGE_SIZE = 10;
+    const offset = searchParams.page * PAGE_SIZE;
+
+    try {
+      const result = await Product.findAndCountAll({
+        where: whereClause,
+        limit: PAGE_SIZE,
+        offset: offset,
+        order: [[`${querySortType}`, `${querySortOrder}`]]
+      });
+
+      const productDtos = result.rows.map(row => new ProductDto(row));
+
+      const summary = new ProductsSummaryDto();
+      summary.totalItems = result.count;
+      summary.currentPage = searchParams.page;
+      summary.totalPages = Math.ceil(result.count / PAGE_SIZE);
+      summary.products = productDtos;
+
+      return summary;
+
+    } catch (error) {
+      console.error('Error fetching products:', error);
+
+      if (retries > 0) {
+        console.log(`Retrying query... attempts left: ${retries}`);
+        await new Promise(resolve => setTimeout(resolve, 200));
+        return getProductsService(searchParams, retries - 1);
+      }
+
+      throw error;
+    };
+  };
